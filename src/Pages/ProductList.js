911c7d201bfd452cb16639d3b0f3d4f4731eedc5
Artur <andrijanov82@gmail.com>
2023-02-14 14:31:26 +0200

    Final commit

--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -15,7 +15,6 @@ class ProductList extends React.Component {
         this.askServerForProducts = this.askServerForProducts.bind(this);
         this.createProducts = this.createProducts.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
-        this.showProducts = this.showProducts.bind(this);
     }
 
     componentDidMount() {
@@ -59,15 +58,6 @@ class ProductList extends React.Component {
         }).then(() => this.askServerForProducts());
     }
 
-    showProducts() {
-        if (this.state.products == null) {
-            return (<div></div>);
-        }
-        else {
-            return this.state.products.map((object, i) => { return <ProductBox product={object} key={i} /> });
-        }
-    }
-
 
     render() {
         let body;
@@ -81,7 +71,7 @@ class ProductList extends React.Component {
         else if (this.state.products.length > 0) {
             body = (
                 <Row className="justify-content-around g-5">
-                    {this.showProducts()}
+                    {this.state.products.map((object, i) => { return <ProductBox product={object} key={i} /> })}
                 </Row>
             );
         }
@@ -96,8 +86,8 @@ class ProductList extends React.Component {
                     <Container fluid className="pb-2 mb-4 border-bottom border-3 border-secondary header">
                         <Row className="align-items-center g-5">
                             <Col><p className="text-center fw-bold fst-italic h1 text-primary">Product List</p></Col>
-                            <Col xs="2"><Button variant="secondary" className="px-4 py-3" onClick={() => this.props.history.push('/add-product')}>Add Products</Button></Col>
-                            <Col xs="2"><Button type="submit" variant="secondary" className="px-4 py-3">Mass Delete</Button></Col>
+                            <Col xs="2"><Button variant="secondary" className="px-4 py-3" onClick={() => this.props.history.push('/add-product')}>ADD</Button></Col>
+                            <Col xs="2"><Button type="submit" variant="secondary" className="px-4 py-3">MASS DELETE</Button></Col>
                         </Row>
                     </Container>
                     <Container fluid>
